8cd8894d8ad393838ebad28f9d11ed46e3869ce0
fvarovillodres <fvarovillodres@gmail.com>
2017-05-06 13:40:22 +0200

    V2.2
    Añadida Public IP al comando ips

--- ips.sh
+++ ips.sh
@@ -3,6 +3,7 @@
 dns=`cat /etc/resolv.conf | grep nameserver | cut -d " " -f 2`
 gw=`ip route show | grep default | awk {'print $3'}`
 nic=`ifconfig |grep Link | sed 's/\s\+/,/g' | cut -d "," -f 1`
+PIP=`curl -s ifconfig.io`
 
 for i in $nic; do
   ip=`ifconfig $i | grep inet | cut -d ":" -f 2 | sed 's/\s\+/,/g' | cut -d "," -f 1`
@@ -11,5 +12,7 @@ for i in $nic; do
 
 
 done
+
+echo -e "Public IP --> $PIP \n"
 
 echo "RED --> DNS:$dns Gateway:$gw"

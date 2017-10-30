d97858403550d216bb5b40e1221ca21ad2bee820
fvarovillodres <fvarovillodres@gmail.com>
2017-10-30 19:51:53 +0100

    V2.3.1
    Arreglado BUG en ips.sh ya que a veces no mostraba la ip publica, ademas se la coloca en ultima posicion por si falla la conexion

--- ips.sh
+++ ips.sh
@@ -3,7 +3,7 @@
 dns=`cat /etc/resolv.conf | grep nameserver | cut -d " " -f 2`
 gw=`ip route show | grep default | awk {'print $3'}`
 nic=`ifconfig |grep Link | sed 's/\s\+/,/g' | cut -d "," -f 1`
-PIP=`curl -s ifconfig.io`
+
 
 for i in $nic; do
   ip=`ifconfig $i | grep inet | cut -d ":" -f 2 | sed 's/\s\+/,/g' | cut -d "," -f 1`
@@ -13,6 +13,10 @@ for i in $nic; do
 
 done
 
-echo -e "Public IP --> $PIP \n"
-
 echo "RED --> DNS:$dns Gateway:$gw"
+
+PIP=`curl -s http://icanhazip.com`
+
+echo 
+
+echo -e "Public IP --> $PIP \n"
--- ips_arch.sh
+++ ips_arch.sh
@@ -3,7 +3,6 @@
 dns=`cat /etc/resolv.conf | grep nameserver | cut -d " " -f 2`
 gw=`ip route show | grep default | awk {'print $3'}`
 nic=`ifconfig |grep flags | sed 's/\s\+/,/g' | cut -d "," -f 1 | tr -d :`
-PIP=`curl -s ifconfig.io`
 
 for i in $nic; do
   ip=`ifconfig $i | grep -e 'inet' | grep -v inet6 | awk '{ print $2 }'`
@@ -13,6 +12,11 @@ for i in $nic; do
 
 done
 
-echo -e "Public IP --> $PIP \n"
 
 echo "RED --> DNS:$dns Gateway:$gw"
+
+PIP=`curl -s http://icanhazip.com`
+
+echo
+
+echo -e "Public IP --> $PIP \n"
